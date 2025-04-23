e3b61bf7635be372253ca910b3fcf27e5f17a120
stationaryaction <kevin.holway@gmail.com>
2025-04-23 11:38:55 -0400

    Implemented database engine setup for SQLite database

---
+++ app/database.py
@@ -0,0 +1,28 @@
+"""Manages the database connections"""
+
+import logging
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+from sqlalchemy.orm import DeclarativeBase
+
+
+# Set up logging
+#
+if __name__ == '__main__':
+    logging.basicConfig(
+        filename='database.log',
+        level=logging.DEBUG,
+        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+    )
+logger = logging.getLogger(__name__)
+
+
+# Set up engine
+#
+db_type = "sqlite"
+db_api = "pysqlite"
+db_name = "movies.db"
+db_url = f"{db_type}+{db_api}:///./{db_name}"
+should_echo = True
+logger.debug(f"Setting up database engine... {db_url}")
+engine = create_engine(db_url, echo=should_echo)
