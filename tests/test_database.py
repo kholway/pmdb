f153cfbd9ce08a04c290e8382706fb2451ee2a3b
stationaryaction <kevin.holway@gmail.com>
2025-04-23 14:45:54 -0400

    Updated commentary in testing

--- tests/test_database.py
+++ tests/test_database.py
@@ -17,7 +17,7 @@ def test_movie_object():
     )
     # Check types
     grail.runtime = "abc" # allowed, even though conflicts with type hint
-    grail.runtime = "91"
+    grail.runtime = "91" # would be nice for this to be converted to right type, as in Pydantic
     grail.mpaa_rating = "PG"  # allowed, even though conflicts with type hint
     grail.mpaa_rating = Rating.PG
     grail.director = "Terry Gilliam, Terry Jones"
