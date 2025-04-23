e9e69aba166ac311e6c8ddbfc4254fad0454daa4
stationaryaction <kevin.holway@gmail.com>
2025-04-23 14:24:43 -0400

    Implemented ORM class Movie using SQLAlchemy

--- app/database.py
+++ app/database.py
@@ -1,9 +1,14 @@
 """Manages the database connections"""
 
 import logging
+from typing import Optional
+import enum
+
 from sqlalchemy import create_engine
+from sqlalchemy import Enum
 from sqlalchemy.orm import Session
-from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import DeclarativeBase, MappedAsDataclass
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 
 # Set up logging
@@ -26,3 +31,39 @@ db_url = f"{db_type}+{db_api}:///./{db_name}"
 should_echo = True
 logger.debug(f"Setting up database engine... {db_url}")
 engine = create_engine(db_url, echo=should_echo)
+
+
+# Create the model
+#
+class Base(MappedAsDataclass, DeclarativeBase):
+    pass
+
+class Rating(enum.Enum):
+    G = "G"
+    PG = "PG"
+    PG13 = "PG-13"
+    R = "R"
+    NC17 = "NC-17"
+    NR = "Not Rated"
+
+
+class Movie(Base):
+    __tablename__ = "Movie"
+
+    title: Mapped[str]
+    
+    id: Mapped[int] = mapped_column(primary_key=True, default=None)
+    year: Mapped[Optional[int]] = mapped_column(default=None)
+    director: Mapped[Optional[str]] = mapped_column(default=None)
+    runtime: Mapped[Optional[int]] = mapped_column(default=None)
+    mpaa_rating: Mapped[Optional[Rating]] = mapped_column(Enum(Rating), default=None)
+
+
+def main():
+    mov = Movie(title="Monty Python and the Holy Grail")
+    mov.runtime = "abc" # Allowed, but conflicts with type hint
+    print(mov)
+
+
+if __name__ == "__main__":
+    main()
--- tests/test_database.py
+++ tests/test_database.py
@@ -2,6 +2,22 @@ import os
 import pytest
 from sqlalchemy import text
 from app.database import engine
+from app.database import Base, Rating, Movie
+
 
 def test_database_connection():
     engine.connect()
+
+def test_movie_object():
+    with pytest.raises(Exception) as e:
+        mov = Movie()
+
+    grail = Movie(
+        title="Monty Python and the Holy Grail",
+    )
+    # Check types
+    grail.runtime = "abc" # allowed, even though conflicts with type hint
+    grail.runtime = "91"
+    grail.mpaa_rating = "PG"  # allowed, even though conflicts with type hint
+    grail.mpaa_rating = Rating.PG
+    grail.director = "Terry Gilliam, Terry Jones"
