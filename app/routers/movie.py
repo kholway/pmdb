268410cf2f9f44876d9fb3ea263effc05db04f71
stationaryaction <kevin.holway@gmail.com>
2025-05-10 17:43:20 -0400

    Add codes and headers to create_movie endpoint

--- app/routers/movie.py
+++ app/routers/movie.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Response
 from sqlalchemy.orm import Session
 from app.schemas.movie import MovieCreate, MovieResponse
 from app.models.movie import Movie
@@ -8,12 +8,18 @@ from app.deps import get_db
 router = APIRouter()
 
 
-@router.post("/movies/", response_model=MovieResponse)
-def create_movie(movie: MovieCreate, db: Session = Depends(get_db)) -> Movie:
+@router.post("/movies/", response_model=MovieResponse, status_code=201)
+def create_movie(
+        movie: MovieCreate, response: Response,
+        db: Session = Depends(get_db)) -> Movie:
     """Adds a movie to the database and returns the orm"""
 
     new_movie = Movie(**movie.model_dump())
     db.add(new_movie)
     db.commit()
-    db.refresh(new_movie)
+    db.refresh(new_movie) # necessary??
+
+    # Add headers
+    response.headers["Location"] = f"/movies/{new_movie.id}"
+
     return new_movie
