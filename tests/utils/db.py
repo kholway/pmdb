b36501aa5bb916baf0334684951b90aaf65da553
stationaryaction <kevin.holway@gmail.com>
2025-05-13 15:24:14 -0400

    Add logging to database testing utility

--- tests/utils/db.py
+++ tests/utils/db.py
@@ -1,12 +1,17 @@
+import logging
+import logging.config
 import sqlalchemy as sa
 import sqlalchemy.orm as sao
 from app.models.movie import Base
 
+logging.config.fileConfig('logging.conf')
+logger = logging.getLogger()
 
 def setup_mem_db() -> tuple[sa.engine.base.Engine, sao.sessionmaker]:    
     """Provide an in-memory database for testing"""
     engine = sa.create_engine("sqlite+pysqlite:///:memory:", echo=True)
     Base.metadata.create_all(engine)
+    logger.debug("Created in-memory database tables!")
     SessionLocal = sao.sessionmaker(engine)
     return engine, SessionLocal
 
